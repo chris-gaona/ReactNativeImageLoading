92a981af911b0053f91afbe7bd9631fe3aeb52cc
Chris Gaona <chrisgaona@Chriss-STS-MacBook-Pro.local>
2019-02-18 14:42:00 -0700

    Sets a default background color while image is loading

--- ProgressiveImage.js
+++ ProgressiveImage.js
@@ -2,11 +2,24 @@ import React, { Component } from 'react';
 import { View, StyleSheet, Image } from 'react-native';
 
 const styles = StyleSheet.create({
-
+  imageOverlay: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    bottom: 0,
+    top: 0,
+  },
+  container: {
+    backgroundColor: '#e1e4e8',
+  },
 });
 
 export default class extends Component {
   render() {
-    return <Image {...this.props} />
+    return (
+      <View style={styles.container}>
+        <Image {...this.props} />
+      </View>
+    )
   }
 }
